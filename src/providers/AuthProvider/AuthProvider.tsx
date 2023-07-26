c099ab854ecca38198dc9532d210ebfcebfee670
Scott <scott@scottbenton.dev>
2023-07-25 22:52:15 -0400

    fix(users): Fixed user names not being set properly for email users

--- src/providers/AuthProvider/AuthProvider.tsx
+++ src/providers/AuthProvider/AuthProvider.tsx
@@ -5,21 +5,31 @@ import { PropsWithChildren, useEffect, useState } from "react";
 import { UserDocument } from "types/User.type";
 import { AuthContext, AUTH_STATE } from "./AuthContext";
 import { clearAnalyticsUser, setAnalyticsUser } from "config/posthog.config";
+import { UserNameDialog } from "components/UserNameDialog";
 
 export function AuthProvider(props: PropsWithChildren) {
   const { children } = props;
   const [authState, setAuthState] = useState<AUTH_STATE>(AUTH_STATE.LOADING);
   const [user, setUser] = useState<UserInfo>();
 
+  const [userNameDialogOpen, setUserNameDialogOpen] = useState(false);
+
   useEffect(() => {
     onAuthStateChanged(
       firebaseAuth,
       (user) => {
         if (user) {
+          if (!user.displayName) {
+            setUserNameDialogOpen(true);
+          }
+
           const userDoc: UserDocument = {
-            displayName: user.displayName ?? "",
-            photoURL: user.photoURL ?? undefined,
+            displayName: user.displayName ?? "Unknown User",
           };
+
+          if (user.photoURL) {
+            userDoc.photoURL = user.photoURL;
+          }
 
           setAnalyticsUser(user);
           updateUserDoc({ uid: user.uid, user: userDoc }).catch((e) => {});
@@ -41,6 +51,10 @@ export function AuthProvider(props: PropsWithChildren) {
 
   return (
     <AuthContext.Provider value={{ user, state: authState }}>
+      <UserNameDialog
+        open={userNameDialogOpen}
+        handleClose={() => setUserNameDialogOpen(false)}
+      />
       {children}
     </AuthContext.Provider>
   );
