f2966f94a4434837da51ea0ff3daf08f6f8aa313
Scott <scott@scottbenton.dev>
2023-08-28 20:45:36 -0400

    feat(campaigns): Fixed issue that would arise if you added a world after adding GMs

--- src/api-calls/campaign/updateCampaignWorld.ts
+++ src/api-calls/campaign/updateCampaignWorld.ts
@@ -1,15 +1,37 @@
-import { deleteField, updateDoc } from "firebase/firestore";
+import { arrayUnion, deleteField, updateDoc } from "firebase/firestore";
 import { getCampaignDoc } from "./_getRef";
 import { createApiFunction } from "api-calls/createApiFunction";
+import { getWorldDoc } from "api-calls/world/_getRef";
 
 export const updateCampaignWorld = createApiFunction<
-  { campaignId: string; worldId?: string },
+  { campaignId: string; gmIds: string[]; worldId?: string },
   void
 >((params) => {
-  const { campaignId, worldId } = params;
+  const { campaignId, gmIds, worldId } = params;
+  if (worldId) {
+    return new Promise((resolve, reject) => {
+      updateDoc(getWorldDoc(worldId), {
+        ownerIds: arrayUnion(...gmIds),
+      })
+        .then(() => {
+          updateDoc(getCampaignDoc(campaignId), {
+            worldId,
+          })
+            .then(() => {
+              resolve();
+            })
+            .catch((e) => {
+              reject(e);
+            });
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  }
   return new Promise((resolve, reject) => {
     updateDoc(getCampaignDoc(campaignId), {
-      worldId: worldId ? worldId : deleteField(),
+      worldId: deleteField(),
     })
       .then(() => {
         resolve();
