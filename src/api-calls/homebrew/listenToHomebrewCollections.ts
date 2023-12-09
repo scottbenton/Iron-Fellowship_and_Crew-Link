a225e50a96e07fb4a66fd2482e794974bd4aad61
Scott <scott@scottbenton.dev>
2023-12-09 01:49:35 -0500

    fix(build): Fixed failing build

--- src/api-calls/homebrew/listenToHomebrewCollections.ts
+++ src/api-calls/homebrew/listenToHomebrewCollections.ts
@@ -1,5 +1,5 @@
 import { onSnapshot, query, where } from "firebase/firestore";
-import { BaseExpansionOrRuleset } from "types/HomebrewCollection";
+import { BaseExpansionOrRuleset } from "types/HomebrewCollection.type";
 import { getHomebrewCollection } from "./_getRef";
 
 export function listenToHomebrewCollections(
